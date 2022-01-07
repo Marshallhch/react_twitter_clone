b04a9469101e68f3455274571136d4c20bd46ebf
한창호 <hanchangho@hanchanghoui-MacBookPro.local>
2022-01-07 13:13:57 +0900

    befor social auth

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 // Switch cant't use in ver 6, so need to down grade react-router-dom to ver 5. I need to study how to use changed react-router-dom.
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,58 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from "react";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    //console.log(event.target.name);
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (newAccount) {
+      console.log("create new account");
+    } else {
+      console.log("log in");
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type='email'
+          placeholder='Email'
+          reauired='true'
+          name='email'
+          value={email || ""}
+          onChange={onChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          requried='true'
+          name='password'
+          value={password || ""}
+          onChange={onChange}
+        />
+        <input type='submit' value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
